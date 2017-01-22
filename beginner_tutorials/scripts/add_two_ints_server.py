4e542e78aeaac9636b785bfe376495efa8e756fa
JaeHP <jayesh.patel19922008@gmail.com>
2017-01-22 18:09:56 -0500

    HW2 Assginment

---
+++ beginner_tutorials/scripts/add_two_ints_server.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+## This code is copied from ROS wiki tutorials http://wiki.ros.org/ROS/Tutorials/WritingServiceClient%28python%29
+from beginner_tutorials.srv import *
+import rospy
+
+def handle_add_two_ints(req):
+    print "Returning [%s + %s = %s]"%(req.a, req.b, (req.a + req.b))
+    return AddTwoIntsResponse(req.a + req.b)
+
+def add_two_ints_server():
+    rospy.init_node('add_two_ints_server')
+    s = rospy.Service('add_two_ints', AddTwoInts, handle_add_two_ints)
+    print "Ready to add two ints."
+    rospy.spin()
+
+if __name__ == "__main__":
+    add_two_ints_server()
